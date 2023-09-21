4cb9cb41edcbcacbc348a296a674924255165a9f
mdalonso <38704285+mdalonso@users.noreply.github.com>
2023-09-21 14:02:02 +0200

    Add files via upload

---
+++ UD1/01-Introduccion/js/index.js
@@ -0,0 +1,15 @@
+
+
+//comentario de una línea
+
+/* comentarios
+de varias
+lineas */
+
+
+
+console.log("Mi primer script en JavaScript"); 
+alert("Buenos días");
+console.log(prompt("Introduzca número"))
+console.log(confirm("¿Desea continuar?"))
+
