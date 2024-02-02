9490ca188dc54601ae2eae41b0420d534073bcd3
mdalonso <38704285+mdalonso@users.noreply.github.com>
2024-02-02 11:51:38 +0100

    Add files via upload

---
+++ assets/librerias/crearXMLHTTP.js
@@ -0,0 +1,13 @@
+function crearConexion(){
+    let objeto;
+    if (window.XMLHttpRequest){
+        objeto=new XMLHttpRequest();
+    }else if (window.ActiveXObject){
+        try{
+            objeto=new ActiveXObject("MSXML2.XMLHTTP");
+        }catch(e){
+            objeto=new ActiveXObject("Microsoft.XMLHTTP");
+        }
+    }
+    return objeto;
+}
