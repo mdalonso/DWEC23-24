3dbeca4630bdbb5cfab436076a44fac83c817a3e
mdalonso <38704285+mdalonso@users.noreply.github.com>
2024-04-08 19:25:17 +0200

    Add files via upload

--- UD10/07_servidorHttpMySQL/db/database.sql
+++ UD10/07_servidorHttpMySQL/db/database.sql
@@ -2,20 +2,20 @@ CREATE DATABASE IF NOT EXISTS empresadb;
 
 USE empresadb;
 
-CREATE TABLE cliente (
+CREATE TABLE clientes (
   id INT(11) NOT NULL AUTO_INCREMENT,
-  nombreCliente VARCHAR(45) DEFAULT NULL,
+  nombreCliente VARCHAR(45) NOT NULL,
   emailCliente VARCHAR(20) DEFAULT NULL, 
   tlfnoCliente VARCHAR(12) NOT NULL,
   empresaCliente VARCHAR(25) DEFAULT NULL, 
   PRIMARY KEY(id)
 );
 
-DESCRIBE cliente;
+DESCRIBE clientes;
 
-INSERT INTO cliente values 
+INSERT INTO clientes values 
   (1, 'Celena Alemán', 'caleman@iestrassierra.com','111111111','Celena S.A.'),
   (2, 'David Bermúdez', 'dbermudez@iestrassierra.com','222222222','David S.L.'),
   (3, 'Dilan Carrillo', 'dcarrillo@iestrassierra.com','333333333','Dilan S.A.');
 
-SELECT * FROM cliente;
+SELECT * FROM clientes;
